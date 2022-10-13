b3bb08faa40acaa1b278f3a20eb15e0fbc8d3be7
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-10-13 23:17:00 +0530

    Added Room and Objects

--- app/src/main/java/com/siddydevelops/instastoryassignment/database/ReelsDAO.kt
+++ app/src/main/java/com/siddydevelops/instastoryassignment/database/ReelsDAO.kt
@@ -10,7 +10,7 @@ interface ReelsDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(item: ReelsItem)
 
-    @Query("UPDATE reels_item SET video_uri=:newAmount WHERE isLiked = :itemName")
+    @Query("UPDATE reels_item SET video_uri=:video_uri WHERE isLiked = :isLiked")
     suspend fun update(video_uri: String, isLiked: Boolean)
 
     @Delete
